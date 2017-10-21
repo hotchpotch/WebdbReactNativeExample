734e6cd37d8450e83aa36f3aa091fbc5bf5f2a68
Yuichi Tateno <hotchpotch@gmail.com>
2017-10-21 19:54:56 +0900

    example code

---
+++ App.js
@@ -0,0 +1,68 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ * @flow
+ */
+
+import React, { Component } from 'react';
+import {
+  Platform,
+  StyleSheet,
+  Text,
+  View
+} from 'react-native';
+import DeviceInfo from "react-native-device-info";
+import Hashes from "jshashes";
+
+const MD5 = new Hashes.MD5();
+console.log(MD5.hex('Hello'));
+
+const instructions = Platform.select({
+  ios: 'Press Cmd+R to reload,\n' +
+    'Cmd+D or shake for dev menu',
+  android: 'Double tap R on your keyboard to reload,\n' +
+    'Shake or press menu button for dev menu',
+});
+
+export default class App extends Component<{}> {
+  render() {
+    return (
+      <View style={styles.container}>
+        <Text style={styles.welcome}>
+          Hello! Locale: {DeviceInfo.getDeviceLocale()}
+        </Text>
+        <Text style={styles.instructions}>
+          To get started, edit App.js
+        </Text>
+        <Text style={styles.instructions}>
+          {instructions}
+        </Text>
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#F5FCFF',
+  },
+  welcome: {
+    fontSize: 20,
+    textAlign: 'center',
+    margin: 20,
+  },
+  instructions: {
+    textAlign: 'center',
+    color: '#333333',
+    marginBottom: 5,
+  },
+});
+
+function square(n: number): number {
+    return n * n;
+}
+square("2");
+
---
+++ __tests__/App.js
@@ -0,0 +1,17 @@
+import 'react-native';
+import React from 'react';
+import App from '../App';
+
+// Note: test renderer must be required after react-native.
+import renderer from 'react-test-renderer';
+
+it('renders correctly', () => {
+  const tree = renderer.create(
+    <App />
+  );
+  expect(tree.toJSON()).toMatchSnapshot();
+});
+
+jest.mock('react-native-device-info', () => ({
+    getDeviceLocale: () => 'ja',
+}));
